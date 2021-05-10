e9bd968394753298fb4ebae4f400c170bcbc74df
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-05-10 19:59:20 -0400

    set up component and connected to app.js

---
+++ src/components/app/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Display from '../color/Display'
+
+export default function App() {
+  return (
+    <>
+      <Display color="#D412F3" /> 
+    </>
+  )
+}
---
+++ src/components/color/Display.jsx
@@ -0,0 +1,8 @@
+import React from 'react'; 
+
+const Display = ({ color }) => ( 
+    <div style={{ background: color, height: '300px',  width: '300px' }}>
+    </div>
+); 
+
+export default Display; 
