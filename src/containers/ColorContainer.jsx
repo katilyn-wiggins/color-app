a8f3abb0542ba6845c6ea2ae2c71676536f31b46
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-05-11 12:17:30 -0400

    random color working and baby picture working

--- src/containers/ColorContainer.jsx
+++ src/containers/ColorContainer.jsx
@@ -1,33 +1,37 @@
 import React, { Component } from 'react'; 
-import Controls from '../components/color/Controls';
 import Display from '../components/color/Display'; 
 
 
-export default class ColorContainer extends Component {
+
+ export default class ColorContainer extends Component {
     state = {
-        boxColor: '#000000'
-    }
-    
-    randomColorPicker() {
-        const newColor =
-       ('#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6));
+        boxColor: '#000000',
+        prevColor: '#000000'
     }
 
     componentDidMount() { 
-        setInterval(randomColorPicker, 1000);
-
+        setInterval( () => this.randomColorPicker(), 1000);
     }
 
-    handleBoxColorChange = (newColor) => {
-        this.setState({ boxColor: newColor });
-        console.log(this.state.boxColor)
+    randomColorPicker() {
+        let randomColor = ('#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6));
+        this.setState({
+            prevColor: this.state.boxColor
+        })
+        console.log(this.state.prevColor); 
+        this.setState({ 
+            boxColor: randomColor
+        })
+        console.log(this.state.boxColor); 
     }
+
     render() {
-        const { boxColor } = this.state; 
-
         return (
             <>
-            <Display boxColor={boxColor} onBox/> 
+            { this.state.boxColor === this.state.prevColor ? 
+            <img src="https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg"/> 
+            : <Display boxColor={this.state.boxColor.toString()} />  }
+            
             </>
         )
     }
